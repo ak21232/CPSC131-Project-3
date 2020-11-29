9366a3f96e0ab1b47bc24ac54be043b2e7e663e9
ak21232 <ak21232@gmail.com>
2020-11-28 19:30:08 -0800

    Update

--- GateControl.hpp
+++ GateControl.hpp
@@ -17,16 +17,16 @@ typedef size_t GateNumber;
 struct Authorization
 {
 	Authorization() { }
-	
+
 	Authorization(CardNumber number, const string& name, const string& startTime, const string& endTime)
 	: number_(number), name_(name), startTime_(startTime), endTime_(endTime) { }
 
 	CardNumber number_;
 
 	string name_;
-	
+
 	string startTime_;
-	
+
 	string endTime_;
 };
 
@@ -43,13 +43,13 @@ struct Transaction
 	: number_(number), name_(name), date_(date), time_(time), accessAllowed_(accessAllowed) { }
 
 	CardNumber number_;
-	
+
 	string name_;
-	
+
 	string date_;
 
 	string time_;
-	
+
 	bool accessAllowed_;
 };
 
@@ -60,24 +60,24 @@ class	GateControl
 {
 	public:
 		bool accessAllowed(CardNumber number);
-	
+
 		bool addAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime);
-		
+
 		bool changeAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime);
-		
+
 		bool deleteAuthorization(CardNumber number);
-		
+
 		void getAllAuthorizations(AuthorizationVector& authorizationVector);
-		
+
 		void getAllTransactions(TransactionVector& transactionVector);
-	
+
 		bool getCardAuthorization(CardNumber number, Authorization& authorization);
 
 		void getCardTransactions(CardNumber number, TransactionVector& transactionVector);
 
 	private:
 		AuthorizationMap authorizationMap_;
-		
+
 		TransactionVector transactionVector_;
 };
 
@@ -98,47 +98,67 @@ extern string gCurrentTime;
 
 bool GateControl::accessAllowed(CardNumber number)
 {
-	
-	//	TO DO
+
+	AuthorizationMapIterator it = authorizationMap_.find(number);
+	if (it != authorizationMap_.end()){
+		return true;
+	}
+	return false;
 
 }
 
 
 bool GateControl::addAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime)
 {
-	
-	//	TO DO
 
+	AuthorizationMapIterator it = authorizationMap_.find(number);
+	if (it != authorizationMap_.end()){
+		return false;
+	}
+	else {
+		authorizationMap_.insert(std::pair<CardNumber,Authorization>(number, Authorization(number, name, startTime,endTime)));
+		return true;
+	}
 }
 
 
 bool GateControl::changeAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime)
 {
-	
-	//	TO DO
+
+	AuthorizationMapIterator it = authorizationMap_.find(number);
+	if (it != authorizationMap_.end()){
+		authorizationMap_.insert(it, std::pair<CardNumber, Authorization>(number, Authorization(number, name, startTime,endTime)));
+		return true;
+	}
+	return false;
 
 }
 
 
 bool GateControl::deleteAuthorization(CardNumber number)
 {
-	
-	//	TO DO
+
+	AuthorizationMapIterator it = authorizationMap_.find(number);
+	if (it != authorizationMap_.end()){
+		authorizationMap_.erase(it);
+		return true;
+	}
+	return false;
 
 }
 
 
 void GateControl::getAllAuthorizations(AuthorizationVector& authorizationVector)
 {
-	
-	//	TO DO	
+
+
 
 }
 
 
 void GateControl::getAllTransactions(TransactionVector& transactionVector)
 {
-	
+
 	//	TO DO
 
 }
@@ -147,14 +167,21 @@ void GateControl::getAllTransactions(TransactionVector& transactionVector)
 bool GateControl::getCardAuthorization(CardNumber number, Authorization& authorization)
 {
 
-	//	TO DO
+	AuthorizationMapIterator it = authorizationMap_.find(number);
+	if (it != authorizationMap_.end()){
+		Authorization(number, authorization.name_, authorization.startTime_,authorization.endTime_);
+		return true;
+	}
+	else {
+		return false;
+	}
 
 }
 
 
 void GateControl::getCardTransactions(CardNumber number, TransactionVector& transactionVector)
 {
-	
-	//	TO DO
+
+
 
 }
